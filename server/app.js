d5d541ca5fac14c6e8179b869b79e6a2716393d3
Arijit Bhattacharya <113541100+Arijitttt@users.noreply.github.com>
2024-03-03 09:22:07 +0530

    mongoose connected

--- server/app.js
+++ server/app.js
@@ -1,11 +1,34 @@
 const express = require('express')
+//const dotenv = require('dotenv')
 const app = express()
+const mongoose = require('mongoose')
+const DB = 'mongodb+srv://abhattacharya030:table@table.rw8gnnh.mongodb.net/?retryWrites=true&w=majority&appName=table'
+
+const saveInDB = async ()=>{
+    await mongoose.connect("mongodb://127.0.0.1/Map")
+    const ProductSchema = new mongoose.Schema({
+        name:String,
+        email:String,
+        age:Number,
+    })
+    const productModel=mongoose.model('testings',ProductSchema)
+    let data=new productModel({
+        name:"Manisha Bhattacharya",
+        email:"bhattacharya@gmail.com",
+        age:22
+    })
+    let result = await data.save()
+    console.log(result);
+}
+saveInDB()
 
 //middleware
 const middleware = (req,resp,next)=>{
     console.log(`this is my middleware`);
     next(); 
 }
+
+
 
 
 app.get('/',(req,resp)=>{
