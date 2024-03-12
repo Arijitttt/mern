2c752e1a52fd173e31c8c9c3a26ff605d1738866
Arijit Bhattacharya <113541100+Arijitttt@users.noreply.github.com>
2024-03-12 20:54:50 +0530

    little bit changes in registration ui

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,12 +1,13 @@
-import React,{useState} from 'react'
-import img1 from '../../assets/regAi.jpg'
-import './signup.css'
+import React, { useState } from "react";
+import img1 from "../../assets/regAi.jpg";
+import "./signup.css";
 import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
 import { MdAlternateEmail } from "react-icons/md";
 import { FaPhone } from "react-icons/fa";
 import { ImProfile } from "react-icons/im";
 import { PiPasswordFill } from "react-icons/pi";
 import { TbPasswordUser } from "react-icons/tb";
+import { Link, NavLink } from 'react-router-dom';
 
 export default function SignUp() {
   const [isFocused, setIsFocused] = useState(false);
@@ -19,52 +20,86 @@ export default function SignUp() {
   };
   return (
     <>
-     <div className="signUp">
+      <div className="signUp">
         <div className="mainBox">
           <div className="left">
             <div className="formWrapper">
               <div className="heading">
-                <h1>SignUp</h1>
+                <h2>SignUp</h2>
               </div>
-              <div className="inputField">
-                <div className="inputs">
-                <MdOutlineDriveFileRenameOutline className='icon'/>
-                <input type="text" name="" id="" placeholder='Your Name' 
-                />
+              <form action="">
+                <div className="inputField">
+                  <div className="inputs">
+                    <MdOutlineDriveFileRenameOutline className="icon" />
+                    <input type="text" name="name" id="name" autoComplete="on" placeholder="Your Name" />
+                  </div>
+                  <div className="inputs">
+                    <MdAlternateEmail className="icon" />
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Your email"
+                      autoComplete="on"
+                    />
+                  </div>
+                  <div className="inputs">
+                    <FaPhone className="icon" />
+                    <input
+                      type="number"
+                      name="number"
+                      id="number"
+                      autoComplete="off"
+                      placeholder="Your phone number"
+                    />
+                  </div>
+                  <div className="inputs">
+                    <ImProfile className="icon" />
+                    <input
+                      type="text"
+                      name="profession"
+                      id="profession"
+                      autoComplete="off"
+                      placeholder="Your profession"
+                    />
+                  </div>
+                  <div className="inputs">
+                    <TbPasswordUser className="icon" />
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      autoComplete="on"
+                      placeholder="Your Password"
+                    />
+                  </div>
+                  <div className="inputs">
+                    <PiPasswordFill className="icon" />
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      autoComplete="off"
+                      placeholder="Confirm your password"
+                    />
+                  </div>
                 </div>
-                <div className="inputs">
-                <MdAlternateEmail className='icon'/>
-                <input type="email" name="" id="" placeholder='Your email'/>
+                <div className="btn">
+                  <input type="submit" name="submit" id="submit" />
                 </div>
-                <div className="inputs">
-                <FaPhone className='icon'/>
-                <input type="tel" name="" id="" placeholder='Your phone number'/>
-                </div>
-                <div className="inputs">
-                <ImProfile className='icon'/>
-                <input type="text" name="" id="" placeholder='Your profession'/>
-                </div>
-                <div className="inputs">
-                <TbPasswordUser className='icon'/>
-                <input type="password" name="" id="" placeholder='Your Password'/>
-                </div>
-                <div className="inputs">
-                <PiPasswordFill className='icon'/>
-                <input type="password" name="" id="" placeholder='Confirm your password'/>
-                </div>
-              </div>
-              <div className="btn">
-                <button>SignUp</button>
-              </div>
+              </form>
             </div>
           </div>
           <div className="right">
             <div className="rightImg">
               <img src={img1} alt="" />
+              <Link to="/login">
+              Already have an account?
+              </Link>
             </div>
           </div>
         </div>
-      </div> 
+      </div>
     </>
-  )
+  );
 }
