2175a09a390cae43414c3f88869811e31efed62b
Arijit Bhattacharya <113541100+Arijitttt@users.noreply.github.com>
2024-03-16 07:33:07 +0530

    home page added

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import "./home.css";
 export default function Home() {
   return (
-    <div>
-      hi from home
-    </div>
-  )
+    <>
+      <div className="homeRoot">
+        {/* <div className="left">
+          <p>Mern</p>
+        </div>
+        <div className="right">
+          <p>Practicing</p>
+        </div> */}
+        <div className="wrapper">
+          <span>Welcome</span>
+          <h3>Mern practicing</h3>
+        </div>
+      </div>
+    </>
+  );
 }
